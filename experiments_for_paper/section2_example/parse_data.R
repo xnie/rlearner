bb5e2a5b9e2a42d957748be512279625fb3640f7
swager <swager@stanford.edu>
2018-06-18 16:21:41 -0700

    Section 2 example.

---
+++ experiments_for_paper/section2_example/parse_data.R
@@ -0,0 +1,22 @@
+rm(list = ls())
+
+# the full dataset is available from
+# https://github.com/gsbDBI/ExperimentData/tree/master/Mobilization/ProcessedData
+data = read.csv("mobilization_no_unlisted 2.csv")
+
+# W is intent to treat
+# contact is received treatment
+
+covariates = c("persons", "state", "county", "competiv",
+               "st_sen", "st_hse", "newreg", "vote98",
+               "vote00", "age", "female")
+X = data[,which(names(data) %in% covariates)]
+W = data$W
+received_treatment = data$contact
+Y = data$vote02
+
+DF = data.frame(X, Y, W)
+
+DF.nona = DF[!is.na(rowSums(DF)),]
+
+write.csv(DF.nona, "data_clean.csv", row.names = FALSE)
